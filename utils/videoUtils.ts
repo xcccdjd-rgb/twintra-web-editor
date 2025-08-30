c1e3615ed14eb52d4af310b106764e9e95fea45a
xcccdjd-rgb <xcccdjd@gmail.com>
2025-08-30 15:22:37 +0700

    Create videoUtils.ts

---
+++ utils/videoUtils.ts
@@ -0,0 +1,4 @@
+export const trimVideo = (videoSrc: string, start: number, end: number) => {
+  // implementasi trimming video nanti (backend / MediaRecorder)
+  console.log(`Trim video ${videoSrc} from ${start} to ${end}`);
+};
