6f1a7a314c7d8382154ac30d1de9788fe4b8a303
xcccdjd-rgb <xcccdjd@gmail.com>
2025-08-30 17:14:08 +0700

    Delete utils directory

--- utils/videoUtils.ts
+++
@@ -1,4 +0,0 @@
-export const trimVideo = (videoSrc: string, start: number, end: number) => {
-  // implementasi trimming video nanti (backend / MediaRecorder)
-  console.log(`Trim video ${videoSrc} from ${start} to ${end}`);
-};
