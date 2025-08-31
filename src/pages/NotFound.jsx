1b54a696ad8ab0b42e20547ad094b7d16666b72c
xcccdjd-rgb <xcccdjd@gmail.com>
2025-08-31 13:22:34 +0700

    Create NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div style={{ padding: "48px", textAlign: "center" }}>
+    <h1>404</h1>
+    <p>Halaman tidak ditemukan.</p>
+  </div>
+);
+
+export default NotFound;
