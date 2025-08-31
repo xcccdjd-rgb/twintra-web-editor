71a21d29c87b85dac2fe8a06248d16e0fce49c36
xcccdjd-rgb <xcccdjd@gmail.com>
2025-08-31 13:25:28 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    &copy; 2025 DSRT &mdash; Digital Smart Revise Technology
+  </div>
+);
+
+export default Footer;
